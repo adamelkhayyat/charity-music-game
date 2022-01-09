492c95a897a0a663dc02b7c9b0a9c5e3018dd16b
Adam Elkhayyat <adamelkhayyat@gmail.com>
2022-01-10 00:47:42 +0100

    First working version with simple time tracking UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
+
+// function responsible for converting milliseconds to seconds
+const msToS = (ms) => ms * 0.001;
+
 function App() {
+
+  let currentInstanceCounter = 0;
+
+  // time is in seconds, so 00:24 => 24, 01:30 => 90
+  const bellInstances = [
+    {
+      time: 24,
+    },
+    {
+      time: 42
+    },
+    {
+      time: 68
+    },
+    {
+      time: 118
+    }
+  ];
+
+  const [soundCaught, setSoundCaught] = useState([]);
+
+  useEffect(() => {
+    let checkedInterval = null;
+    let startTime = null;
+    let endTime = null;
+    const musicPlayer = document.getElementById('music-player');
+
+    // set up listener for space key
+    document.addEventListener('keydown', event => {
+      if (event.code === 'Space' && startTime !== null) {
+        console.log('Space pressed');
+        endTime = new Date().getTime();
+        const responseTime = (endTime - startTime) / 1000;
+
+        console.log("time taken:", responseTime, "secs!");
+
+        const state = {
+          time: responseTime,
+          soundAt: bellInstances[currentInstanceCounter - 1].time.toFixed(2),
+          iteration: currentInstanceCounter - 1
+        };
+        setSoundCaught(arr => [...arr, state])
+
+        // reset timers
+        startTime = null;
+        endTime = null;
+      }
+    })
+
+    musicPlayer.addEventListener('play', (event) => {
+      console.log("Music is going! 🎷");
+      console.log(musicPlayer.currentTime);
+
+      console.log("bell details:", bellInstances[currentInstanceCounter].time.toFixed(2));
+
+      // start measuring...
+      checkedInterval = setInterval(function () {
+          // check if we're reaching a point in the music where a special sound is played
+          if (bellInstances[currentInstanceCounter].time.toFixed(2) === musicPlayer.currentTime.toFixed(2)) {
+            // start timer to calculate response time
+            startTime = new Date().getTime();
+            console.log("Bell rings!");
+
+            // move on to next (possible) special sound to look out for
+            if (currentInstanceCounter < (bellInstances.length - 1)) currentInstanceCounter++;
+          }
+      }, 5);
+    });
+
+    musicPlayer.addEventListener('pause', (event) => {
+      console.log("Music has been paused! ⏸");
+      clearInterval(checkedInterval);
+    });
+
+    musicPlayer.addEventListener('ended', (event) => {
+      console.log("Music has ended! 👋");
+      clearInterval(checkedInterval);
+    });
+  },);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <figure>
+        <figcaption>Listen for the bell 🔔</figcaption>
+        <audio
+            id="music-player"
+            controls
+            src="/media/bell-sample.mp3">
+          Your browser does not support the <code>audio</code> element.
+        </audio>
+      </figure>
+      { soundCaught.map(sc => <p>Bell number {sc.iteration}, originally played at {sc.soundAt}, was caught within {sc.time} seconds.</p>)}
     </div>
   );
 }
